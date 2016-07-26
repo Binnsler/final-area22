9a367edf8ed70838b9fcf0c137906f6e97b3af80
marczenn <jamie.m.binns@gmail.com>
2016-07-26 10:07:25 -0600

    trying again

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,12 +2,10 @@ import axios from 'axios';
 import {browserHistory} from 'react-router';
 import {EMAIL_ERROR, MEMBER_ERROR, SET_MEMBER, GET_MEMBERS} from './types';
 
+var ROOT_URL = 'http://localhost:3090';
+
 if(process.env.NODE_ENV === 'production'){
   var ROOT_URL = '';
-}
-else{
-  var ROOT_URL = 'http://localhost:3090';
-}
 
 console.log(`${ROOT_URL}/email`)
 // Pull in data from /contact form and send off to Sendgrid API
