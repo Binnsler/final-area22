ed8df1fc7c66b13c72f5285ea0333a28be22a7d4
marczenn <jamie.m.binns@gmail.com>
2016-10-07 15:30:36 -0600

    forgot links

--- src/components/home.js
+++ src/components/home.js
@@ -132,8 +132,8 @@ class Home extends Component {
           <div className="community-callout">
             <h3 className="community-text">We are excited to share that one of our members, Mallory Sabado, just released her album out into the world. Check it out!</h3>
             <div>
-              <button className="solid-black-button">CD Baby</button>
-              <button className="solid-black-button">Google Store</button>
+              <button className="solid-black-button" href="https://goo.gl/YO1pKw">CD Baby</button>
+              <button className="solid-black-button" href="https://goo.gl/ePJCxK">Google Store</button>
             </div>
           </div>
 
