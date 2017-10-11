0560a2f77bb28c197bc72c14bf4ab2ff763fbbcf
Kristin Yen <KristinYen@Kristins-MacBook-Pro.local>
2017-10-11 05:37:27 -0400

    Added callout to SBS

--- src/pages/soapboxsouls.js
+++ src/pages/soapboxsouls.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import members from "../data/members.json";
+import CommunityCallout from "../components/Home/community-callout.js";
 
 
 class SoapboxSouls extends Component{
@@ -33,6 +34,12 @@ class SoapboxSouls extends Component{
               <div className="artworks-text">
                   <p className="paragraph">Soapbox Souls is a group of multi-talented musicians who span generations and genres with their eclectic style. Be prepared to take an exciting journey through Rock, Pop, Blues, R&B, Country, and Americana while immersing yourself in truthful lyrics, intricately woven harmonies and clever song craft. Their sound is organic, full and effective, enhanced by the vocals of the four female singers that yield a tantalizing soundscape very few can replicate. From thought-provoking songs to sophisticated production power, Soapbox Souls sets the stage for music&#39;s newest renaissance.</p>
               </div>
+              <CommunityCallout
+                text={ "Soapbox Souls will be performing November 18th at 7pm in Dennisport at the Cape Cod Community Media Center. Tickets are $12 online and $15 at the door. Check it out!" }
+                buttons={ [
+                    { "url": "http://livefromcenterstage.com/product/soapbox-souls-nov-18-7pm-live-from-center-stage/", "text": "Tickets" }
+                ] }
+              />
               { this.renderMembersList() }
             </div>
         );
